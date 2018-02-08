503031a78e26ce9704f8c05dcdce7afef4592015
yangjiqi <yang.jiq@husky.neu.edu>
2018-02-08 12:55:19 -0800

    sample init

---
+++ server/app.js
@@ -0,0 +1,17 @@
+/**
+ * Created by sesha on 7/27/17.
+ */
+
+/* the function is a Java script constructor which will be instantiated from top level server.js */
+/* req = parses the req from http, parses and makes it a nice clean object */
+// params is part of the request. any variables in the path will be available as a a map in params
+
+module.exports = function (app) {
+	var connectionString = 'mongodb://127.0.0.1:27017/test'; // for local
+	   if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
+	var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment var password = process.env.MLAB_PASSWORD_WEBDEV;
+	connectionString = 'mongodb://' + username + ':' + password; 
+	connectionString += ' @ds229468.mlab.com:29468/heroku_dhdtp04k '; // use yours
+	}
+};
+
